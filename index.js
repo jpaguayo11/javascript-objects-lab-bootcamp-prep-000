a516b9276490cfd065473204245d0bf1619556e6
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-05 01:43:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var recipes = {muffins: "Bananas and flour"}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = {}
-  Object.assign (newObject, object, {[key]: value})
-  return newObject
+  return Object.assign ({}, object, {[key]: value})
 }
